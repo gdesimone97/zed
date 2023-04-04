6eb9ee99ddca4c55d7459a2194b34d65b6e3d3ba
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-04-04 13:10:11 +0200

    Update run_container.bash

--- docker_ros/custom/run_container.bash
+++ docker_ros/custom/run_container.bash
@@ -3,5 +3,5 @@
 CURR=$(pwd)/..
 sudo docker run --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 --runtime nvidia --network=host --privileged \
--v /dev:/dev -v $CURR:/root -it \
+-v /dev:/dev -v $CURR:/root/share -it \
 --name zed_cnt stereolabs/zed:3.8-py-runtime-l4t-r35.1
