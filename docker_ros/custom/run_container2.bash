1784b8fed30e8f63f9c1502ac6c33fc35a9dd7ef
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-04-04 13:12:08 +0200

    Update run_container2.bash

--- docker_ros/custom/run_container2.bash
+++ docker_ros/custom/run_container2.bash
@@ -3,5 +3,5 @@
 CURR=$(pwd)/..
 sudo docker run --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 --runtime nvidia --network=host --privileged \
--v /dev:/dev -v $CURR:/app -it \
+-v /dev:/dev -v $CURR:/root/share -it \
 --name zed_cnt zed_ros_noetic
